90c891e967471c346e76280d8ce7c5db5189a146
Asen Mitrev <asen.mitrev@mentormate.com>
2023-05-03 21:28:58 +0300

    Done

--- client/src/components/App/App.tsx
+++ client/src/components/App/App.tsx
@@ -1,20 +1,48 @@
-import {useState} from 'react';
-import axios from "axios";
+import { useState, useEffect } from "react";
 import PromptInput from "../PromptInput/PromptInput";
-import './App.css';
-import {ResponseInterface} from "../PromptResponseList/response-interface";
+import "./App.css";
+import { ResponseInterface } from "../PromptResponseList/response-interface";
 import PromptResponseList from "../PromptResponseList/PromptResponseList";
 
-type ModelValueType = 'gpt' | 'codex' | 'image';
-const App = () => {
+const BestAnswer = ["Тук", "не", "е", "информация!"];
+const firstAnswer = [
+  "Здравейте!",
+  "Аз",
+  "съм",
+  "езиковият",
+  "модел",
+  "на",
+  "НАП",
+  "и",
+  "съм",
+  "вашият",
+  "ИИ",
+  "асистент",
+  "днес.",
+  "Трениран",
+  "съм",
+  "на",
+  "милиарди",
+  "въпроси",
+  "към",
+  "служители",
+  "на",
+  "НАП",
+  "и",
+  "техните",
+  "отговори.",
+  "Как",
+  "мога",
+  "да",
+  "ви",
+  "помогна",
+  "днес?",
+];
 
+const App = () => {
   const [responseList, setResponseList] = useState<ResponseInterface[]>([]);
-  const [prompt, setPrompt] = useState<string>('');
-  const [promptToRetry, setPromptToRetry] = useState<string | null>(null);
-  const [uniqueIdToRetry, setUniqueIdToRetry] = useState<string | null>(null);
-  const [modelValue, setModelValue] = useState<ModelValueType>('gpt');
+  const [prompt, setPrompt] = useState<string>("");
   const [isLoading, setIsLoading] = useState(false);
-  let loadInterval: number | undefined;
 
   const generateUniqueId = () => {
     const timestamp = Date.now();
@@ -22,158 +50,95 @@ const App = () => {
     const hexadecimalString = randomNumber.toString(16);
 
     return `id-${timestamp}-${hexadecimalString}`;
-  }
-
-  const htmlToText = (html: string) => {
-    const temp = document.createElement('div');
-    temp.innerHTML = html;
-    return temp.textContent;
-  }
+  };
+  useEffect(() => {
+    getGPTResult("", firstAnswer);
+  }, []);
 
   const delay = (ms: number) => {
-    return new Promise( resolve => setTimeout(resolve, ms) );
-  }
-
-  const addLoader = (uid: string) => {
-    const element = document.getElementById(uid) as HTMLElement;
-    element.textContent = ''
-
-    // @ts-ignore
-    loadInterval = setInterval(() => {
-      // Update the text content of the loading indicator
-      element.textContent += '.';
-
-      // If the loading indicator has reached three dots, reset it
-      if (element.textContent === '....') {
-        element.textContent = '';
-      }
-    }, 300);
-  }
-
+    return new Promise((resolve) => setTimeout(resolve, ms));
+  };
 
   const addResponse = (selfFlag: boolean, response?: string) => {
-    const uid = generateUniqueId()
-    setResponseList(prevResponses => [
+    const uid = generateUniqueId();
+    setResponseList((prevResponses) => [
       ...prevResponses,
       {
         id: uid,
         response,
-        selfFlag
+        selfFlag,
       },
     ]);
     return uid;
-  }
+  };
 
-  const updateResponse = (uid: string, updatedObject: Record<string, unknown>) => {
-    setResponseList(prevResponses => {
-      const updatedList = [...prevResponses]
+  const updateResponse = (
+    uid: string,
+    updatedObject: Record<string, unknown>
+  ) => {
+    setResponseList((prevResponses) => {
+      const updatedList = [...prevResponses];
       const index = prevResponses.findIndex((response) => response.id === uid);
       if (index > -1) {
         updatedList[index] = {
           ...updatedList[index],
-          ...updatedObject
-        }
+          ...updatedObject,
+        };
       }
       return updatedList;
     });
-  }
+  };
 
-  const regenerateResponse = async () => {
-    await getGPTResult(promptToRetry, uniqueIdToRetry);
-  }
-
-  const getGPTResult = async (_promptToRetry?: string | null, _uniqueIdToRetry?: string | null) => {
-    // Get the prompt input
-    const _prompt = _promptToRetry ?? htmlToText(prompt);
-
-    // If a response is already being generated or the prompt is empty, return
-    if (isLoading || !_prompt) {
-      return;
-    }
-
+  const getGPTResult = async (prompt: string, answer: string[]) => {
+    if (isLoading) return;
     setIsLoading(true);
 
     // Clear the prompt input
-    setPrompt('');
+    setPrompt("");
 
     let uniqueId: string;
-    if (_uniqueIdToRetry) {
-      uniqueId = _uniqueIdToRetry;
-    } else {
-      // Add the self prompt to the response list
-      addResponse(true, _prompt);
-      uniqueId = addResponse(false);
-      await delay(50);
-      addLoader(uniqueId);
-    }
+    // Add the self prompt to the response list
+    prompt && addResponse(true, prompt);
+    uniqueId = addResponse(false);
+    await delay(50);
 
     try {
-      // Send a POST request to the API with the prompt in the request body
-      const response = await axios.post('get-prompt-result', {
-        prompt: _prompt,
-        model: modelValue
-      });
-      if (modelValue === 'image') {
-        // Show image for `Create image` model
+      let responseSoFar = "";
+      for (const word of answer) {
+        responseSoFar += ` ${word}`;
+        await delay(200);
         updateResponse(uniqueId, {
-          image: response.data,
-        });
-      } else {
-        updateResponse(uniqueId, {
-          response: response.data.trim(),
+          response: responseSoFar,
         });
       }
-
-      setPromptToRetry(null);
-      setUniqueIdToRetry(null);
     } catch (err) {
-      setPromptToRetry(_prompt);
-      setUniqueIdToRetry(uniqueId);
-      updateResponse(uniqueId, {
-        // @ts-ignore
-        response: `Error: ${err.message}`,
-        error: true
-      });
     } finally {
       // Clear the loader interval
-      clearInterval(loadInterval);
       setIsLoading(false);
     }
-  }
+  };
 
   return (
     <div className="App">
       <div id="response-list">
-        <PromptResponseList responseList={responseList} key="response-list"/>
-      </div>
-      { uniqueIdToRetry &&
-        (<div id="regenerate-button-container">
-          <button id="regenerate-response-button" className={isLoading ? 'loading' : ''} onClick={() => regenerateResponse()}>
-            Regenerate Response
-          </button>
-        </div>
-        )
-      }
-      <div id="model-select-container">
-        <label htmlFor="model-select">Select model:</label>
-        <select id="model-select" value={modelValue} onChange={(event) => setModelValue(event.target.value as ModelValueType)}>
-          <option value="gpt">GPT-3 (Understand and generate natural language )</option>
-          <option value="codex">Codex (Understand and generate code, including translating natural language to code)
-          </option>
-          <option value="image">Create Image (Create AI image using DALL·E models)</option>
-        </select>
+        <PromptResponseList responseList={responseList} key="response-list" />
       </div>
       <div id="input-container">
         <PromptInput
           prompt={prompt}
-          onSubmit={() => getGPTResult()}
+          onSubmit={() => getGPTResult(prompt, BestAnswer)}
           key="prompt-input"
           updatePrompt={(prompt) => setPrompt(prompt)}
         />
-        <button id="submit-button" className={isLoading ? 'loading' : ''} onClick={() => getGPTResult()}></button>
+        <button
+          id="submit-button"
+          disabled={isLoading}
+          className={isLoading ? "loading" : ""}
+          onClick={() => getGPTResult(prompt, BestAnswer)}
+        ></button>
       </div>
     </div>
   );
-}
+};
 
 export default App;
--- client/src/components/PromptResponseList/PromptResponseList.tsx
+++ client/src/components/PromptResponseList/PromptResponseList.tsx
@@ -1,10 +1,9 @@
-import React, {FC, useEffect, useRef} from 'react';
-import ChatGptImg from '../../img/chatgpt.png';
-import MyImg from '../../img/me.png';
-import ReactMarkdown from 'react-markdown';
-import {ResponseInterface} from "./response-interface";
-import hljs from 'highlight.js';
-import './PromptResponseList.css';
+import { FC, useEffect, useRef } from "react";
+import ChatGptImg from "../../img/chatgpt.png";
+import MyImg from "../../img/me.png";
+import { ResponseInterface } from "./response-interface";
+import hljs from "highlight.js";
+import "./PromptResponseList.css";
 
 interface PromptResponseListProps {
   responseList: ResponseInterface[];
@@ -15,7 +14,7 @@ const PromptResponseList: FC<PromptResponseListProps> = ({ responseList }) => {
 
   useEffect(() => {
     hljs.highlightAll();
-  })
+  });
 
   useEffect(() => {
     hljs.highlightAll();
@@ -24,26 +23,25 @@ const PromptResponseList: FC<PromptResponseListProps> = ({ responseList }) => {
   return (
     <div className="prompt-response-list" ref={responseListRef}>
       {responseList.map((responseData) => (
-        <div className={"response-container " + (responseData.selfFlag ? 'my-question' : 'chatgpt-response')} key={responseData.id}>
-          <img className="avatar-image" src={responseData.selfFlag ? MyImg : ChatGptImg} alt="avatar"/>
-          <div className={(responseData.error ? 'error-response ' : '') + "prompt-content"} id={responseData.id}>
-            { responseData.image &&
-                <img src={responseData.image} className="ai-image" alt="generated ai"/>
+        <div
+          className={
+            "response-container " +
+            (responseData.selfFlag ? "my-question" : "chatgpt-response")
+          }
+          key={responseData.id}
+        >
+          <img
+            className="avatar-image"
+            src={responseData.selfFlag ? MyImg : ChatGptImg}
+            alt="avatar"
+          />
+          <div
+            className={
+              (responseData.error ? "error-response " : "") + "prompt-content"
             }
-            { responseData.response &&
-              <ReactMarkdown
-                children={responseData.response ?? ''}
-                components={{
-                  code({className, children}) {
-                    return(
-                      <code className={className}>
-                        {children}
-                      </code>
-                    )
-                  }
-                }}
-              />
-            }
+            id={responseData.id}
+          >
+            {responseData.response}
           </div>
         </div>
       ))}
